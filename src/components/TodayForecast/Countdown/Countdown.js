21fff0b9a874286fcb6eb154af000b9356ffba1d
kastommor1 <kastommor@gmail.com>
2024-02-25 16:05:07 +0200

    Countdown style

--- src/components/TodayForecast/Countdown/Countdown.js
+++ src/components/TodayForecast/Countdown/Countdown.js
@@ -3,19 +3,19 @@ import classes from "./Countdown.module.css"
 const Countdown = () => {
     return (
         <div className={classes.countdown}>
-            <div>
+            <div className={classes.column}>
                 <p>30</p>
                 <p>days</p>
             </div>
-            <div>
+            <div className={classes.column}>
                 <p>16</p>
                 <p>hours</p>
             </div>
-            <div>
+            <div className={classes.column}>
                 <p>15</p>
                 <p>minutes</p>
             </div>
-            <div>
+            <div className={classes.column}>
                 <p>35</p>
                 <p>seconds</p>
             </div>
