67b2260c6f49b2c733a3bc95ca2779de4f41cda2
kastommor1 <kastommor@gmail.com>
2024-02-26 16:49:05 +0200

    Main style done

--- src/components/RangeForecast/RangeForecast.js
+++ src/components/RangeForecast/RangeForecast.js
@@ -1,13 +1,20 @@
+import IconWeather from "../IconWeather/IconWeather";
 import classes from "./RangeForecast.module.css"
 
-const RangeForecast = ()=>{
-    return(
+const RangeForecast = () => {
+    const arr = [1,2,3,4,5,6,7]
+    return (
         <div className={classes['range-forecast']}>
             <h2>Week</h2>
             <ul>
-                <li>Monday</li>
-                <li>Tuesday</li>
-                <li>Wednesday</li>
+                {arr.map(item=>(
+                          <li key={item}>
+                          <div className={classes.day}>Monday</div>
+                          <IconWeather icon="cloudy" className={classes.icon} />
+                          <div className={classes.temperature}>28°/29°</div>
+                      </li>
+                ))}
+          
             </ul>
         </div>
     )
